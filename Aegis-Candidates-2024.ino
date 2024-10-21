10c1eaad11c5ece1b50e32e7ac3e094ebd1d2df7
Ángel Eduardo López Solís <a00837012@tec.mx>
2024-10-21 17:34:15 -0600

    Added

--- Aegis-Candidates-2024.ino
+++ Aegis-Candidates-2024.ino
@@ -36,9 +36,9 @@ static const uint8_t FRONT_ULTRASONIC_PIN = 10;
 static const uint8_t LEFT_ULTRASONIC_PIN = 11;
 static const uint8_t RIGHT_ULTRASONIC_PIN = 12;
 
-NewPing frontUltrasonic();
-NewPing leftUltrasonic();
-NewPing rightUltrasonic();
+NewPing frontUltrasonic = NewPing(FRONT_ULTRASONIC_PIN, FRONT_ULTRASONIC_PIN);
+NewPing leftUltrasonic = NewPing(LEFT_ULTRASONIC_PIN, LEFT_ULTRASONIC_PIN);
+NewPing rightUltrasonic = NewPing(RIGHT_ULTRASONIC_PIN, RIGHT_ULTRASONIC_PIN);
 
 // Color
 
@@ -46,17 +46,17 @@ Adafruit_TCS34725 color = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34
 
 // Infrared
 
-static const uint8_t WEST_IR_PIN = 13;
-static const uint8_t NORT_WEST_IR_PIN = A0;
+static const uint8_t WEST_IR_PIN = A0;
 static const uint8_t NORTH_IR_PIN = A1;
-static const uint8_t NORTH_EAST_IR_PIN = A2;
-static const uint8_t EAST_IR_PIN = A3;
+static const uint8_t EAST_IR_PIN = A2;
 
 // Servo
 
 static const uint8_t GRIPPER_PIN = 9;
+static const uint8_t GRIPPER_ARM_PIN = 10;
 
 Servo gripper;
+Servo gripper_arm;
 
 // Motor
 
@@ -77,14 +77,67 @@ void setup()
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
     
+    pinMode(WEST_IR_PIN, INPUT);
+    pinMode(NORTH_IR_PIN, INPUT);
+    pinMode(EAST_IR_PIN, INPUT);
+    
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
+    
+    gripper.attach(GRIPPER_PIN);
+    Serial.begin(9600);
+    
+    color.begin();
 }
 
 void loop()
 {
+    // Serial.print("Detects object at ");
+    // Serial.println(leftUltrasonic.ping_cm());
+    float r, g, b = 0;
+    color.getRGB(&r, &g, &b);
+    Serial.print("Colors: ");
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
+    
+    // stepforward();
+    // delay(1000);
+    // stepBack();
+    
+    // digitalWrite(LED_BUILTIN, HIGH);
+    // delay(100);
+    // digitalWrite(LED_BUILTIN, LOW);
+    // delay(100);
+    // digitalWrite(LED_BUILTIN, HIGH);
+    // delay(100);
+    // digitalWrite(LED_BUILTIN, LOW);
+    
+    // delay(1000);
+    // digitalWrite(LED_BUILTIN, digitalRead(NORT_WEST_IR_PIN));
+    // delay(1000);
+    
+    // digitalWrite(LED_BUILTIN, HIGH);
+    // delay(100);
+    // digitalWrite(LED_BUILTIN, LOW);
+    
+    // delay(1000);
+    // digitalWrite(LED_BUILTIN, digitalRead(NORTH_IR_PIN));
+    // delay(1000);
+    
+    // gripper.write(0);
+    // delay(1000);
+    // gripper.write(90);
+    // delay(1000);
+    
+    
+    
+    
     
 }
 
