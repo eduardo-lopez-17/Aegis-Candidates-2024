30cf023a8e63b7ea30d5f54fa39af753cdc56c0c
Ángel Eduardo López Solís <a00837012@tec.mx>
2024-10-21 18:08:26 -0600

    Added Colors library

---
+++ lib/Colors.h
@@ -0,0 +1,20 @@
+/**
+ * Colors.h - Library used to define and simplify colors identification.
+ * Autor: Angel Eduardo López.
+ */
+
+#ifndef COLORS_H
+#define COLORS_H
+
+enum Colors
+{
+      RED = 0,
+      GREEN,
+      BLUE,
+      CYAN,
+      YELLOW,
+      MAGENTA,
+      PURPLE
+};
+
+#endif
